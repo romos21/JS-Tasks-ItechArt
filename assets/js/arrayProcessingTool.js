2e4c6726b1fd1f6fae1fc82ee6d018d9e4ed3c5f
romos21 <romos2606@gmal.com>
2020-11-05 18:07:09 +0300

    fix deffects with array processing tools

--- assets/js/arrayProcessingTool.js
+++ assets/js/arrayProcessingTool.js
@@ -21,25 +21,16 @@ const objectArrProcessTools = {
         resultHTMLElement.style.backgroundColor = '#56e360';
         return true;
     },
-    negativeNumbersCheck: function (){
-        const arrTmpM = this.arr.filter(el => el < 0);
-        if (arrTmpM.length === this.arr.length) {
-            let result = this.search().split('').filter(el => Number(el));
-            return -result[0];
-        }
-        return null;
-    },
     subSumOn1: function () {
-        const negativeNumbersAll=this.negativeNumbersCheck();
-        if(negativeNumbersAll){
-            return negativeNumbersAll;
-        }
         let arrTmp = this.arr;
         let maxSum = 0;
         let currSum = 0;
         for (let i = 0; i < arrTmp.length; i++) {
+            console.log(currSum,arrTmp[i]);
             if (currSum + arrTmp[i] >= 0) {
                 currSum += arrTmp[i];
+            }else {
+                currSum=0;
             }
             if (currSum > maxSum) {
                 maxSum = currSum;
@@ -48,10 +39,6 @@ const objectArrProcessTools = {
         return maxSum;
     },
     subSumOn2: function () {
-        const negativeNumbersAll=this.negativeNumbersCheck();
-        if(negativeNumbersAll){
-            return negativeNumbersAll;
-        }
         let sum = 0;
         let currentSum = 0;
         for (let i = 0; i < this.arr.length; i++) {
@@ -91,53 +78,22 @@ const objectArrProcessTools = {
         return sum;
     },
     search: function () {
-        let arrTmp = [];
-        arrTmp = this.arr.map(el => {
-            if (!arrTmp.includes(el)) {
-                return el;
-            }
-        })
-        console.log(arrTmp);
-        const arrLength = arrTmp.length;
-        if (!arrLength) {
-            console.log('array is empty');
-            return 'array is empty';
-        }
-        let max = arrTmp[0], min = arrTmp[0];
-        let canFindMiddle = arrLength % 2;
-        let mid = canFindMiddle ? arrTmp[0] : '';
-        if (arrLength === 1) {
-            return 'max: ' + arrTmp[0] + ' min: ' + arrTmp[0] + ' middle:' + arrTmp[0];
-        }
-        if (arrLength === 2) {
-            if (arrTmp[1] > arrTmp[0]) {
-                return 'max: ' + arrTmp[1] + ' min: ' + arrTmp[0] + ' middle:' + mid;
-            }
-            return 'max: ' + arrTmp[0] + ' min: ' + arrTmp[1] + ' middle:' + mid;
-        }
-        for (let i = 1; i < arrLength; i++) {
-            let lessElementsLength = 0, greatElementsLength = 0;
-            for (let j = 0; j < arrLength && canFindMiddle; j++) {
-                console.log('arrTmp[i]:' + arrTmp[i], 'arrTmp[j]:' + arrTmp[j]);
-                if (j !== i) {
-                    if (arrTmp[j] < arrTmp[i]) {
-                        lessElementsLength++;
-                    } else {
-                        greatElementsLength++;
-                    }
-                }
-            }
-            if (lessElementsLength === greatElementsLength) {
-                mid = arrTmp[i];
-            }
-            if (this.arr[i] > max) {
-                max = this.arr[i];
-            }
-            if (this.arr[i] < min) {
-                min = this.arr[i];
+        const arrTmp=this.arr,
+            arrLength=arrTmp.length;
+        for(let i=1;i<arrLength;i++) {
+            for (let j = i; j > 0 && arrTmp[j - 1] > arrTmp[j]; j--) {
+                [arrTmp[j - 1], arrTmp[j]] = [arrTmp[j], arrTmp[j - 1]]
             }
         }
-        return 'max: ' + max + ' min: ' + min + ' middle:' + mid;
+
+        let median=0;
+
+        if(arrLength%2===0){
+            median=(arrTmp[arrLength/2]+arrTmp[arrLength/2-1])/2;
+        }else {
+            median=arrTmp[Math.floor(arrLength/2)];
+        }
+        return 'max: ' + arrTmp[arrLength-1] + ' min: ' + arrTmp[0] + ' median:' + median;
     },
     sequence: function () {
         let resultSequenceLength = 1;
